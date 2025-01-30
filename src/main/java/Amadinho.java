357af6e928893bb7b5a2ec3710826821b8afda46
b1inmeister <jlmk2102@gmail.com>
2025-01-30 17:43:44 +0800

    Update code to echo inputs
    Import Scanner class for inputs
    Create function echo() that takes input then prints it
    Modify intro and exit messages to functions intro() and exit()
    Put all functions into main()

--- src/main/java/Amadinho.java
+++ src/main/java/Amadinho.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Amadinho {
-    public static void main(String[] args) {
+
+    public static void intro() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Amadinho!");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void echo(Scanner in, boolean quit) {
+        while (!quit) {
+            String command = in.nextLine();
+
+            if (command.equals("bye")) {
+                quit = true;
+            } else {
+                System.out.println("___________________________________________________________");
+                System.out.println(command);
+                System.out.println("____________________________________________________________");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean quit = false;
+
+        intro();
+        echo(in, quit);
+        exit();
     }
 }
