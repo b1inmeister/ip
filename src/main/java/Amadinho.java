c072fee6919a8f8744690a520c2a4b73024b32d4
b1inmeister <jlmk2102@gmail.com>
2025-01-30 18:38:24 +0800

    Update code with list functionality
    Create a new array of Strings called toDo
    Modify echo() to conduct specific commands
    Add function add() to put instructions into toDo
    Add function list() to print all contents in toDo

--- src/main/java/Amadinho.java
+++ src/main/java/Amadinho.java
@@ -15,26 +15,60 @@ public class Amadinho {
         System.out.println("____________________________________________________________");
     }
 
-    public static void echo(Scanner in, boolean quit) {
+    public static void readInput(Scanner in, boolean quit, String[] toDo) {
         while (!quit) {
             String command = in.nextLine();
 
-            if (command.equals("bye")) {
+            switch (command) {
+            case "bye":
                 quit = true;
-            } else {
-                System.out.println("___________________________________________________________");
-                System.out.println(command);
-                System.out.println("____________________________________________________________");
+                break;
+            case "list":
+                list(toDo);
+                break;
+            default:
+                add(toDo, command);
             }
         }
+    }
+
+    public static void add(String[] toDo, String command) {
+        for (int i = 0; i < toDo.length; i++) {
+            if (toDo[i] == null) {
+                toDo[i] = command;
+                break;
+            }
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + command);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void list(String[] toDo) {
+        int counter = 1;
+
+        System.out.println("____________________________________________________________");
+
+        for (int i = 0; i < toDo.length; i++) {
+            if (toDo[i] == null) {
+                break;
+            } else {
+                System.out.println(counter + ". " + toDo[i]);
+                counter++;
+            }
+        }
+
+        System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         boolean quit = false;
+        String[] toDo = new String[100];
 
         intro();
-        echo(in, quit);
+        readInput(in, quit, toDo);
         exit();
     }
 }
