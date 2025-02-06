4579fd0aa2ceb457f82d29566ea8d73d26f9cd5a
b1inmeister <jlmk2102@gmail.com>
2025-02-06 16:06:42 +0800

    Edit toString() in Task class
    Remove one space from returned String in toString()

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,7 +9,7 @@ public class Task {
 
     @Override
     public String toString() {
-        return " [" + getStatusIcon() + "] " + description;
+        return "[" + getStatusIcon() + "] " + description;
     }
 
     public String getStatusIcon() {
