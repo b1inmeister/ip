458288a1144cde5827490782e5fd5640da836a0a
b1inmeister <jlmk2102@gmail.com>
2025-02-07 00:36:37 +0800

    Edit main code for refactoring Round 1
    Move messages and errors to separate methods
    Convert magic strings and numbers to constants
    Modify execution of commands executeCommand()
    Modify printing list of tasks commandList()
    Modify addition of Todo tasks commandTodo()
    Add insertion of task into list as method

--- src/main/java/Amadinho.java
+++ src/main/java/Amadinho.java
@@ -1,178 +1,232 @@
 import java.util.Scanner;
 
 public class Amadinho {
-    public static void readInput(Scanner in, boolean isFinished, Task[] toDoList) {
-        while (!isFinished) {
-            String command = in.nextLine();
+    public static final int LIST_MAX_VALUE = 100;
+    public static final int LIST_COUNTER_START = 1;
+    public static final int ARRAY_INCREMENT = 1;
+    public static final int COUNTER_START = 0;
 
-            // trigger for mark / unmark
-            if (command.startsWith("mark")) {
-                mark(toDoList, command);
-            } else if (command.startsWith("unmark")) {
-                unmark(toDoList, command);
-            } else {
-                // trigger for other commands (add / list / bye)
-                switch (command) {
-                case "bye":
-                    isFinished = true;
-                    break;
-                case "list":
-                    list(toDoList);
-                    break;
-                default:
-                    add(toDoList, command);
-                }
-            }
-        }
-    }
+    public static final String COMMAND_BYE = "bye";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_MARK = "mark";
+    public static final String COMMAND_UNMARK = "unmark";
+    public static final String COMMAND_TODO = "todo";
+    public static final String COMMAND_DEADLINE = "deadline";
+    public static final String COMMAND_EVENT = "event";
 
-    public static void mark(Task[] toDoList, String command) {
-        int numberPosition = command.indexOf(" ");
-        int number = Integer.parseInt(command.substring(numberPosition + 1)) - 1;
+    public static final String MESSAGE_WELCOME_LINE1 = "Hello! I'm Amadinho!";
+    public static final String MESSAGE_WELCOME_LINE2 = "What can I do for you?";
+    public static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!";
 
-        // counter for do while loop
-        int i = 0;
+    public static final String MESSAGE_LIST_INTRO = "Here are the tasks in your list:";
+    public static final String MESSAGE_MARK_COMPLETE = "Nice! I've marked this task as done:";
+    public static final String MESSAGE_UNMARK_COMPLETE = "OK, I've marked this task as not done yet:";
+    public static final String MESSAGE_ADDED_TASK = "Got it. I've added this task:";
 
-        do {
-            // exception for out of bounds
-            if (toDoList[i] == null) {
-                throw new IndexOutOfBoundsException("Index out of bounds");
-            }
+    public static final String MESSAGE_ERROR_INVALID_COMMAND = "Invalid command. Please try again.";
+    public static final String MESSAGE_ERROR_OUTOFBOUNDS = "Number provided is not in the list.";
 
-            if (i == number) {
-                toDoList[i].markAsDone();
-                break;
-            }
+    public static final String BORDER_LINE = "____________________________________________________________";
 
-            i++;
-        } while (i <= number);
-
-        System.out.println("____________________________________________________________");
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println((i + 1) + "." + toDoList[i]);
-        System.out.println("____________________________________________________________");
-    }
-
-    public static void unmark(Task[] toDoList, String command) {
-        int numberPosition = command.indexOf(" ");
-        int number = Integer.parseInt(command.substring(numberPosition + 1)) - 1;
-
-        // counter for do while loop
-        int i = 0;
-
-        do {
-            // exception for out of bounds
-            if (toDoList[i] == null) {
-                throw new IndexOutOfBoundsException("Index out of bounds");
-            }
-
-            if (i == number) {
-                toDoList[i].markAsUndone();
-                break;
-            }
-
-            i++;
-        } while (i <= number);
-
-        System.out.println("____________________________________________________________");
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println((i + 1) + "." + toDoList[i]);
-        System.out.println("____________________________________________________________");
-    }
-
-    public static void add(Task[] toDoList, String command) {
-        for (int i = 0; i < toDoList.length; i++) {
-            if (toDoList[i] == null) {
-                if (command.contains("deadline")) {
-                    int indexDeadline = command.indexOf("deadline");
-                    int indexBy = command.indexOf("/by");
-
-                    String description = command.substring(indexDeadline + 9, indexBy - 1);
-                    String by = command.substring(indexBy + 4);
-
-                    Deadline newDeadline = new Deadline(description, by);
-                    toDoList[i] = newDeadline;
-
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + newDeadline);
-                    System.out.println("Now you have " + (i + 1) + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                    break;
-                } else if (command.contains("event")) {
-                    int indexEvent = command.indexOf("event");
-                    int indexFrom = command.indexOf("/from");
-                    int indexTo = command.indexOf("/to");
-
-                    String description = command.substring(indexEvent + 6, indexFrom - 1);
-                    String from = command.substring(indexFrom + 6, indexTo - 1);
-                    String to = command.substring(indexTo + 4);
-
-                    Event newEvent = new Event(description, from, to);
-                    toDoList[i] = newEvent;
-
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + newEvent);
-                    System.out.println("Now you have " + (i + 1) + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                    break;
-                } else if (command.contains("todo")) {
-                    int indexTodo = command.indexOf("todo");
-
-                    String description = command.substring(indexTodo + 5);
-
-                    Todo newTodo = new Todo(description);
-                    toDoList[i] = newTodo;
-
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + newTodo);
-                    System.out.println("Now you have " + (i + 1) + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                    break;
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Invalid command. Please try again.");
-                    System.out.println("____________________________________________________________");
-                    break;
-                }
-            }
-        }
-    }
-
-    public static void list(Task[] toDoList) {
-        int counter = 1;
-
-        System.out.println("____________________________________________________________");
-        System.out.println("Here are the tasks in your list:");
-
-        for (int i = 0; i < toDoList.length; i++) {
-            if (toDoList[i] == null) {
-                break;
-            } else {
-                System.out.println(counter + "." + toDoList[i]);
-                counter++;
-            }
-        }
-
-        System.out.println("____________________________________________________________");
-    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean isFinished = false;
-        Task[] toDoList = new Task[100];
+        Task[] taskList = new Task[LIST_MAX_VALUE];
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Amadinho!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        welcomeMessage();
 
-        readInput(in, isFinished, toDoList);
+        while (true) {
+            String userCommand = in.next();
+            String information = in.nextLine().trim();
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+            if (userCommand.equals(COMMAND_BYE)) {
+                break;
+            }
+
+            executeCommand(taskList, userCommand, information);
+        }
+
+        exitMessage();
+    }
+
+
+    /*
+     * COMMAND-RELATED FUNCTIONS
+     */
+
+    public static void executeCommand(Task[] taskList, String userCommand, String information) {
+        switch (userCommand) {
+        case COMMAND_LIST:
+            commandList(taskList);
+            break;
+        case COMMAND_MARK:
+            commandMark(taskList);
+            break;
+        case COMMAND_UNMARK:
+            commandUnmark(taskList);
+            break;
+        case COMMAND_TODO:
+            commandTodo(taskList, information);
+            break;
+        case COMMAND_DEADLINE:
+            commandDeadline(taskList, information);
+            break;
+        case COMMAND_EVENT:
+            commandEvent(taskList, information);
+            break;
+        default:
+            errorInvalidCommand();
+            break;
+        }
+    }
+
+    public static void commandList(Task[] taskList) {
+        int taskCounter = LIST_COUNTER_START;
+
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_LIST_INTRO);
+
+        for (int arrayCounter = COUNTER_START; arrayCounter < taskList.length; arrayCounter++) {
+            if (taskList[arrayCounter] == null) {
+                break;
+            } else {
+                System.out.println(taskCounter + "." + taskList[arrayCounter]);
+                taskCounter++;
+            }
+        }
+
+        System.out.println(BORDER_LINE);
+    }
+
+    // broken
+    public static void commandMark(Task[] taskList) {
+        Scanner in = new Scanner(System.in);
+        int taskPosition = in.nextInt();
+
+        // counter for do while loop
+        int arrayCounter = COUNTER_START;
+
+        do {
+            if (taskList[arrayCounter] == null) {
+                errorOutOfBounds();
+            }
+
+            if (arrayCounter == taskPosition - ARRAY_INCREMENT) {
+                taskList[arrayCounter].markAsDone();
+                break;
+            }
+
+            arrayCounter++;
+        } while (arrayCounter <= taskPosition);
+
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_MARK_COMPLETE);
+        System.out.println((taskPosition) + "." + taskList[arrayCounter]);
+        System.out.println(BORDER_LINE);
+    }
+
+    // broken
+    public static void commandUnmark(Task[] taskList) {
+        Scanner in = new Scanner(System.in);
+        int taskPosition = in.nextInt();
+
+        // counter for do while loop
+        int arrayCounter = COUNTER_START;
+
+        do {
+            if (taskList[arrayCounter] == null) {
+                errorOutOfBounds();
+            }
+
+            if (arrayCounter == taskPosition - ARRAY_INCREMENT) {
+                taskList[arrayCounter].markAsUndone();
+                break;
+            }
+
+            arrayCounter++;
+        } while (arrayCounter <= taskPosition);
+
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_UNMARK_COMPLETE);
+        System.out.println((taskPosition) + "." + taskList[arrayCounter]);
+        System.out.println(BORDER_LINE);
+    }
+
+    public static void commandTodo(Task[] taskList, String information) {
+        Todo newTodo = new Todo(information);
+
+        insertIntoTaskList(taskList, newTodo);
+    }
+
+
+
+    // incomplete
+    public static void commandDeadline(Task[] taskList, String information) {
+        Todo newTodo = new Todo(information);
+
+        insertIntoTaskList(taskList, newTodo);
+    }
+
+    // incomplete
+    public static void commandEvent(Task[] taskList, String information) {
+        Todo newTodo = new Todo(information);
+
+        insertIntoTaskList(taskList, newTodo);
+    }
+
+    public static void insertIntoTaskList(Task[] taskList, Todo newTodo) {
+        for (int arrayCounter = COUNTER_START; arrayCounter < taskList.length; arrayCounter++) {
+            if (taskList[arrayCounter] == null) {
+                taskList[arrayCounter] = newTodo;
+                completeMessage(newTodo, arrayCounter);
+                break;
+            }
+        }
+    }
+
+
+    /*
+     * MESSAGE-RELATED METHODS
+     */
+
+    public static void welcomeMessage() {
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_WELCOME_LINE1);
+        System.out.println(MESSAGE_WELCOME_LINE2);
+        System.out.println(BORDER_LINE);
+    }
+
+    public static void exitMessage() {
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_EXIT);
+        System.out.println(BORDER_LINE);
+    }
+
+    public static void completeMessage(Task newTask, int arrayCounter) {
+        int totalTasks = arrayCounter + ARRAY_INCREMENT;
+
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_ADDED_TASK);
+        System.out.println("  " + newTask);
+        System.out.println(printTotalTasks(totalTasks));
+        System.out.println(BORDER_LINE);
+    }
+
+    public static String printTotalTasks(int totalTasks) {
+        return "Now you have " + totalTasks + " tasks in the list.";
+    }
+
+    /*
+     * ERROR-RELATED METHODS
+     */
+
+    public static void errorOutOfBounds() {
+        throw new IndexOutOfBoundsException(MESSAGE_ERROR_OUTOFBOUNDS);
+    }
+
+    public static void errorInvalidCommand() {
+        System.out.println(BORDER_LINE);
+        System.out.println(MESSAGE_ERROR_INVALID_COMMAND);
+        System.out.println(BORDER_LINE);
     }
 }
