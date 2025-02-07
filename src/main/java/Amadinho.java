7bec1cc12ca15b14755e24f974c8292156427c8b
b1inmeister <jlmk2102@gmail.com>
2025-02-07 16:57:19 +0800

    Edit main code for refactoring Round 3
    Add constants for command identifiers and their length
    Modify printing list loop to enhanced for loop
    Rework deadline and event methods
    Refactor string manipulation to new methods
    Edit welcome message to simpler version

--- src/main/java/Amadinho.java
+++ src/main/java/Amadinho.java
@@ -5,6 +5,7 @@ public class Amadinho {
     public static final int LIST_COUNTER_START = 1;
     public static final int ARRAY_INCREMENT = 1;
     public static final int COUNTER_START = 0;
+    public static final int START_OF_STRING = 0;
 
     public static final String COMMAND_BYE = "bye";
     public static final String COMMAND_LIST = "list";
@@ -14,14 +15,27 @@ public class Amadinho {
     public static final String COMMAND_DEADLINE = "deadline";
     public static final String COMMAND_EVENT = "event";
 
-    public static final String MESSAGE_WELCOME_LINE1 = "Hello! I'm Amadinho!";
-    public static final String MESSAGE_WELCOME_LINE2 = "What can I do for you?";
-    public static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!";
+    public static final String IDENTIFIER_BY = "/by";
+    public static final String IDENTIFIER_FROM = "/from";
+    public static final String IDENTIFIER_TO = "/to";
+
+    public static final int LENGTH_BY = IDENTIFIER_BY.length();
+    public static final int LENGTH_FROM = IDENTIFIER_FROM.length();
+    public static final int LENGTH_TO = IDENTIFIER_TO.length();
+
+    public static final String MESSAGE_WELCOME = """
+            To add new Tasks                    todo / deadline / event
+            To mark Tasks as complete or not    mark / unmark
+            To list all Tasks                   list
+            To give up and exit                 bye""";
+    public static final String MESSAGE_EXIT = "Congrats! You have given up.";
 
     public static final String MESSAGE_LIST_INTRO = "Here are the tasks in your list:";
     public static final String MESSAGE_MARK_COMPLETE = "Nice! I've marked this task as done:";
     public static final String MESSAGE_UNMARK_COMPLETE = "OK, I've marked this task as not done yet:";
     public static final String MESSAGE_ADDED_TASK = "Got it. I've added this task:";
+    public static final String MESSAGE_TOTALTASKS_FRONT = "Now you have ";
+    public static final String MESSAGE_TOTALTASKS_BACK = " tasks in the list.";
 
     public static final String MESSAGE_ERROR_INVALID_COMMAND = "Invalid command. Please try again.";
     public static final String MESSAGE_ERROR_OUTOFBOUNDS = "Number provided is not in the list.";
@@ -98,11 +112,11 @@ public class Amadinho {
     public static void printList(Task[] taskList) {
         int taskCounter = LIST_COUNTER_START;
 
-        for (int arrayCounter = COUNTER_START; arrayCounter < taskList.length; arrayCounter++) {
-            if (taskList[arrayCounter] == null) {
+        for (Task task : taskList) {
+            if (task == null) {
                 break;
             } else {
-                System.out.println(taskCounter + "." + taskList[arrayCounter]);
+                System.out.println(taskCounter + "." + task);
                 taskCounter++;
             }
         }
@@ -145,30 +159,65 @@ public class Amadinho {
 
     public static void commandTodo(Task[] taskList, String information) {
         Todo newTodo = new Todo(information);
-
         insertIntoTaskList(taskList, newTodo);
     }
     
-    // incomplete
     public static void commandDeadline(Task[] taskList, String information) {
-        Todo newTodo = new Todo(information);
-        insertIntoTaskList(taskList, newTodo);
+        if (isMissing(information, IDENTIFIER_BY)) {
+            errorInvalidCommand();
+            return;
+        }
+
+        int descriptionPosition = findIndex(information, IDENTIFIER_BY);
+
+        String description = generateSubstring(information, START_OF_STRING, descriptionPosition);
+        String by = generateSubstring(information, descriptionPosition + LENGTH_BY);
+
+        Deadline newDeadline = new Deadline(description, by);
+        insertIntoTaskList(taskList, newDeadline);
     }
 
-    // incomplete
     public static void commandEvent(Task[] taskList, String information) {
-        Todo newTodo = new Todo(information);
-        insertIntoTaskList(taskList, newTodo);
+        if (isMissing(information, IDENTIFIER_FROM) || isMissing(information, IDENTIFIER_TO)) {
+            errorInvalidCommand();
+            return;
+        }
+
+        int descriptionPosition = findIndex(information, IDENTIFIER_FROM);
+        int toPosition = findIndex(information, IDENTIFIER_TO);
+
+        String description = generateSubstring(information, START_OF_STRING, descriptionPosition);
+        String from = generateSubstring(information, descriptionPosition + LENGTH_FROM, toPosition);
+        String to = generateSubstring(information, toPosition + LENGTH_TO);
+
+        Event newEvent = new Event(description, from, to);
+        insertIntoTaskList(taskList, newEvent);
     }
 
-    public static void insertIntoTaskList(Task[] taskList, Todo newTodo) {
+    public static void insertIntoTaskList(Task[] taskList, Task newTask) {
         for (int arrayCounter = COUNTER_START; arrayCounter < taskList.length; arrayCounter++) {
             if (taskList[arrayCounter] == null) {
-                taskList[arrayCounter] = newTodo;
-                addCommandMessage(newTodo, arrayCounter);
+                taskList[arrayCounter] = newTask;
+                addCommandMessage(newTask, arrayCounter);
                 break;
             }
         }
+    }
+
+    public static boolean isMissing(String information, String identifier) {
+        return !(information.contains(identifier));
+    }
+
+    public static int findIndex(String information, String identifier) {
+        return information.indexOf(identifier);
+    }
+
+    public static String generateSubstring(String information, int start, int end) {
+        return information.substring(start, end).trim();
+    }
+
+    public static String generateSubstring(String information, int start) {
+        return information.substring(start).trim();
     }
 
 
@@ -178,8 +227,7 @@ public class Amadinho {
 
     public static void welcomeMessage() {
         System.out.println(BORDER_LINE);
-        System.out.println(MESSAGE_WELCOME_LINE1);
-        System.out.println(MESSAGE_WELCOME_LINE2);
+        System.out.println(MESSAGE_WELCOME);
         System.out.println(BORDER_LINE);
     }
 
@@ -213,7 +261,7 @@ public class Amadinho {
     }
 
     public static String printTotalTasks(int totalTasks) {
-        return "Now you have " + totalTasks + " tasks in the list.";
+        return MESSAGE_TOTALTASKS_FRONT + totalTasks + MESSAGE_TOTALTASKS_BACK;
     }
 
 
@@ -225,6 +273,7 @@ public class Amadinho {
         throw new IndexOutOfBoundsException(MESSAGE_ERROR_OUTOFBOUNDS);
     }
 
+    // tip: use switch statement for customised messages
     public static void errorInvalidCommand() {
         System.out.println(BORDER_LINE);
         System.out.println(MESSAGE_ERROR_INVALID_COMMAND);
